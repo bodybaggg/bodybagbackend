4b53df687f29b182ffa540010e4443befa6ae1c1
Akash P S <116643229+ItzmeAkash@users.noreply.github.com>
2024-01-11 18:18:37 +0530

    admin view setuped

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from . models import User,CategoryName
 
+class BodyBagAdmin(admin.ModelAdmin):
+    list_display =('id','name','gender','email','phone_number','instagram','location','category','experience','unique_code','is_staff','is_superuser','is_active')
+    list_display_links = ('id','name')
+    search_fields = ('name',)
+    list_per_page = 10
+    list_editable = ("is_staff","is_active",)
+    
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User,BodyBagAdmin)
 admin.site.register(CategoryName)

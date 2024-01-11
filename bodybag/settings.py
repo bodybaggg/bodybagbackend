cc80fb521ff69c4be1cf56a350eee0520b1dbe68
Akash P S <116643229+ItzmeAkash@users.noreply.github.com>
2024-01-11 16:31:40 +0530

    vercel deplyment setup is done

--- bodybag/settings.py
+++ bodybag/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static" 
+# STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static" 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -181,3 +181,6 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
+
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')

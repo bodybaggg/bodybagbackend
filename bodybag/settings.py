917b6260449fd0778a932482f11b5548e110e053
Akash P S <116643229+ItzmeAkash@users.noreply.github.com>
2024-02-04 15:56:09 +0530

    added

--- bodybag/settings.py
+++ bodybag/settings.py
@@ -182,5 +182,7 @@ SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')

3059d13eb5ca76db09d6a3e532cb92706eebdcc9
Akash P S <116643229+ItzmeAkash@users.noreply.github.com>
2024-02-04 16:34:11 +0530

    settings updated removed staticfiles

--- bodybag/settings.py
+++ bodybag/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+# STATIC_URL = "static/"
 # STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static" 
 
 # Default primary key field type
@@ -182,7 +182,7 @@ SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static'),
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
